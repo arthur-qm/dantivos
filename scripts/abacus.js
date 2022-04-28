8e290a4f81618af362abff1d8f7b60aa7144a336
Arthur Queiroz <arthurquimu@gmail.com>
2022-04-28 18:27:15 -0300

    Add clicking behaviour

--- scripts/abacus.js
+++ scripts/abacus.js
@@ -31,7 +31,7 @@ for (let i = 0; i < ROWS; i++) {
     for (let j = 0; j < COLS; j++) {
         let obj = document.createElement('div');
         obj.setAttribute('class', 'unity');
-        obj.setAttribute('id', `o${i}${j}`);
+        obj.setAttribute('id', `o${i}-${j}`);
         obj.style.backgroundColor = `rgb(${colors[i][0]}, ${colors[i][1]}, ${colors[i][2]})`;
 
         r.appendChild(obj); 
@@ -40,3 +40,35 @@ for (let i = 0; i < ROWS; i++) {
     abacus.appendChild(r);
     
 }
+
+function change(row, column) {
+
+    let fila = abacus.querySelector(`#r${row}`);
+    let pushedguy = fila.querySelector('.push');
+    let obj_at = fila.querySelector(`#o${row}-${column}`);
+
+    if (pushedguy === null || pushedguy === undefined) {
+        obj_at.classList.add('push');
+        lefts[row] = column;
+        return;
+    }
+
+    pushedguy.classList.remove('push');
+
+    if (lefts[row]-1 >= column) {
+        obj_at.classList.add('push');
+        
+        lefts[row] = column;
+    } else {
+
+        if (column+1 < COLS) {
+            fila.querySelector(`#o${row}-${column+1}`).classList.add('push');
+        }
+
+        lefts[row] = column + 1;
+    }
+}
+
+for (let i = 0; i < ROWS; i++)
+    for (let j = 0; j < COLS; j++)
+        abacus.querySelector(`#o${i}-${j}`).setAttribute('onclick', `change(${i}, ${j});`);
