f6a14ff6cd8c2671a91e8e80e1ebfa82b44275b9
Arthur Queiroz <arthurquimu@gmail.com>
2022-04-28 17:50:38 -0300

    Add random colors

--- scripts/abacus.js
+++ scripts/abacus.js
@@ -2,9 +2,22 @@ const ROWS = 10;
 const COLS = 10;
 
 var lefts = [];
+var colors = [];
 
 for (let i = 0; i < ROWS; i++) {
     lefts.push(COLS);
+}
+
+for (let i = 0; i < ROWS; i++) {
+    
+    let c = [];
+
+    for (let j = 0; j < 3; j++) {
+        c.push(Math.floor(256 * Math.random()));
+    }
+
+    colors.push(c);
+
 }
 
 var abacus = document.getElementById('simu')
@@ -19,6 +32,8 @@ for (let i = 0; i < ROWS; i++) {
         let obj = document.createElement('div');
         obj.setAttribute('class', 'unity');
         obj.setAttribute('id', `o${i}${j}`);
+        obj.style.backgroundColor = `rgb(${colors[i][0]}, ${colors[i][1]}, ${colors[i][2]})`;
+
         r.appendChild(obj); 
     }
 
