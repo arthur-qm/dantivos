3fc22777700ce12443caa03692f86fb3ced0cb57
Arthur Queiroz <arthurquimu@gmail.com>
2022-05-01 00:06:51 -0300

    Deal with images

--- scripts/napier.js
+++ scripts/napier.js
@@ -1,5 +1,13 @@
 var napier = document.querySelector('#simu');
 var inputs = napier.querySelector('.inputs');
+var displays = napier.querySelector('.displays');
+var urls = [];
+
+for (let i = 0; i < 10; i++) {
+    urls.push(`./imgs/napier/col${i}.png`);
+}
+
+urls.push('./imgs/napier/colnull.png');
 
 for (let i = 0; i < 11; i++) {
     
@@ -7,6 +15,7 @@ for (let i = 0; i < 11; i++) {
     numi.setAttribute('class', 'btn btn-outline-dark');
     numi.setAttribute('id', `n${i}`);
     numi.setAttribute('type', 'button');
+    numi.setAttribute('onclick', `addcol(${i});`)
     
     if (i < 10) {
         numi.innerHTML = `${i}`;
@@ -16,3 +25,8 @@ for (let i = 0; i < 11; i++) {
 
     inputs.appendChild(numi);
 }
+
+let col_ini = document.createElement('img');
+col_ini.setAttribute('src', urls[urls.length-1])
+
+displays.appendChild(col_ini);
