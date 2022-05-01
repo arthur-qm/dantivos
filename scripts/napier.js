a8be8602f9b5a747605fe8fe0f32411754cc3f11
Arthur Queiroz <arthurquimu@gmail.com>
2022-04-30 23:05:51 -0300

    Add buttons

--- scripts/napier.js
+++ scripts/napier.js
@@ -1,5 +1,18 @@
-var napier = document.querySelector('.simu');
+var napier = document.querySelector('#simu');
+var inputs = napier.querySelector('.inputs');
 
-for (let i = 0; i < 10; i++) {
+for (let i = 0; i < 11; i++) {
     
+    let numi = document.createElement('button');
+    numi.setAttribute('class', 'btn btn-outline-dark');
+    numi.setAttribute('id', `n${i}`);
+    numi.setAttribute('type', 'button');
+    
+    if (i < 10) {
+        numi.innerHTML = `${i}`;
+    } else {
+        numi.innerHTML = 'X';
+    }
+
+    inputs.appendChild(numi);
 }
