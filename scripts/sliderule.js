aeee4740d91408fa303aac5e2b397e471cf7806c
Murilo Valentim Zabott <murilo.zabott@gmail.com>
2022-06-12 20:37:56 -0300

    suporte mobile p/ regua de calculo

--- scripts/sliderule.js
+++ scripts/sliderule.js
@@ -3,26 +3,13 @@ let images = document.querySelectorAll("img")
 let ruleTop = document.querySelector("#top")
 let ruleMiddle = document.querySelector("#middle")
 let ruleBottom = document.querySelector("#bottom")
-let cursor = document.querySelector("#cursor")
 let metal = document.querySelectorAll(".metal")
 
 let btnFlip = document.querySelector("#flip")
 
-let rulex = ruleMiddle.getBoundingClientRect().left, cursorx = cursor.getBoundingClientRect().left, flip = false;
-
 images.forEach(img => {
   img.setAttribute("draggable", false)
 })
-
-function dragrule({ movementX }) {
-  rulex += movementX;
-  ruleMiddle.style.left = `${rulex}px`
-}
-
-function dragcursor({ movementX }) {
-  cursorx += movementX;
-  cursor.style.left = `${cursorx}px`
-}
 
 function fliprule(){
   flip = !flip;
@@ -41,17 +28,29 @@ function fliprule(){
   })
 }
 
-ruleMiddle.addEventListener("mousedown", () => {
-  window.addEventListener("mousemove", dragrule)
-})
-
-cursor.addEventListener("mousedown", () => {
-  window.addEventListener("mousemove", dragcursor)
-})
-
-window.addEventListener("mouseup", () => {
-  window.removeEventListener("mousemove", dragrule)
-  window.removeEventListener("mousemove", dragcursor)
-})
-
 btnFlip.addEventListener("click", fliprule)
+
+document.querySelectorAll(".draggable").forEach(el => {
+  el.onmousedown = e => {
+    const offsetX = e.clientX - parseInt(el.style.left);
+
+    window.onmousemove = ev => {
+      el.style.left = ev.clientX - offsetX + "px";
+    }
+
+    window.onmouseup = () => {
+      window.onmousemove = null;
+      window.onmouseup = null;
+    }
+  }
+
+  el.ontouchstart = e => {
+    let location = e.targetTouches[0];
+    const offsetX = location.clientX - parseInt(el.style.left);
+
+    el.ontouchmove = e => {
+      let movement = e.targetTouches[0];
+      el.style.left = movement.clientX - offsetX + "px";
+    }
+  }
+})
