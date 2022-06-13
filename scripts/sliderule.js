852df7b4cd5a3481c528dc99130eda2ea89cd676
Arthur Queiroz <arthurquimu@gmail.com>
2022-06-13 14:36:35 -0300

    Fix slide rule again

--- scripts/sliderule.js
+++ scripts/sliderule.js
@@ -6,6 +6,7 @@ let ruleBottom = document.querySelector("#bottom")
 let metal = document.querySelectorAll(".metal")
 
 let btnFlip = document.querySelector("#flip")
+let flip = 0
 
 images.forEach(img => {
   img.setAttribute("draggable", false)
@@ -13,18 +14,21 @@ images.forEach(img => {
 
 function fliprule(){
   flip = !flip;
-  
   ruleTop.children[0].setAttribute("src", !flip ? "./imgs/sliderule/cima1.jpg" : "./imgs/sliderule/cima2.jpg")
   
-  ruleMiddle.style.top = `${!flip ? 77 : 92}px`
+  ruleMiddle.style.top = `${parseInt(ruleMiddle.style.top.slice(0, -2)) + 15 * ( 2 * flip - 1)}px`
   ruleMiddle.children[0].setAttribute("src", !flip ? "./imgs/sliderule/meio1.jpg" : "./imgs/sliderule/meio2.jpg")
   
-  ruleBottom.style.top = `${!flip ? 77 + 133 : 92 + 133}px`
+  ruleBottom.style.top = `${parseInt(ruleBottom.style.top.slice(0, -2)) + 15 * ( 2 * flip - 1)}px`
   ruleBottom.children[0].setAttribute("src", !flip ? "./imgs/sliderule/baixo1.jpg" : "./imgs/sliderule/baixo2.jpg")
 
   metal.forEach(div => {
-    div.style.top = `${!flip ? 77 : 92}px`
-    div.style.left ? div.style.left = `${-4 * flip}px` : div.style.right = `${4 * flip}px`
+    div.style.top = `${parseInt(div.style.top.slice(0, -2)) + 14 * ( 2 * flip - 1)}px`
+    if (div.id !== "mdireito") {
+      div.style.left = `${-4 * flip}px`
+    } else {
+      div.style.left = `${parseInt(div.style.left.slice(0, -2)) - 4*(2 * flip - 1)}px`
+    }
   })
 }
 
